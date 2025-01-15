b8c8a954fcd7e8aaa9fa6cbb08894bef5aeb5eb3
Luke Maurits <luke@maurits.id.au>
2025-01-15 17:24:02 +0100

    Expand model to include sex effects, fit with and without these effects.

--- analysis/fit_model.R
+++ analysis/fit_model.R
@@ -4,14 +4,18 @@ library(rstan)
 d <- read_csv("../data/raw_data_anonymized.csv")
 
 demog_tab <- d %>%
-	select(pid, age, culture) %>%
+	select(pid, sex, age, culture) %>%
 	unique %>%
 	arrange(pid) %>%
-	mutate(culture = as.integer(as.factor(culture)))	# Bandongo = 1, Bayaka = 2
+	mutate(culture = as.integer(as.factor(culture)),
+	       sex = if_else(sex == "MALE", -0.5, 0.5))
+# Bandongo = 1, Bayaka = 2
+# Male = -0.5, Female = 0.5
 
 stan_data = list(
                            N_data = nrow(d),
                            N_part = length(unique(d$pid)),
+                           sex = pull(demog_tab, sex),
                            age = pull(demog_tab, age),
                            culture = pull(demog_tab, culture),
                            part_id = d$pid,
@@ -20,8 +24,18 @@ stan_data = list(
 		   )
 
 stan_code <- read_file("stan_code.c")
+
+# With sex effects
 model <- stan(model_code = stan_code, data=stan_data,
 	  warmup=7500, iter=15000, chains=4, cores=4,
 	  control=list(adapt_delta=0.95)
 		 )
-saveRDS(model, "model.rds")
+saveRDS(model, "model_sex.rds")
+
+# Without sex effects
+stan_data$sex <- rep(0, stan_data$N_part)
+model <- stan(model_code = stan_code, data=stan_data,
+	  warmup=7500, iter=15000, chains=4, cores=4,
+	  control=list(adapt_delta=0.95)
+		 )
+saveRDS(model, "model_no_sex.rds")
--- analysis/stan_code.c
+++ analysis/stan_code.c
@@ -1,19 +1,24 @@
-data{
+data {
     // Participant data
     int N_part;
-    real age[N_part];
     int  culture[N_part];
+    real age[N_part];
+    real<lower=-0.5, upper=0.5> sex[N_part];
 
     // Task data
     int N_data;
     int<lower=1, upper=N_part> part_id[N_data];
     int<lower=1, upper=2> condition[N_data];
     int<lower=0, upper=1> shared[N_data];
+
 }
 
 parameters{
     real<lower=0, upper=1> start_prob;
-    vector<lower=0, upper=1>[4] end_prob;
+    vector<lower=0, upper=1>[4] end_probs;
+    vector[4] end_prob_sex_effects;
+    vector[4] age_param_1_sex_effects;
+    vector[4] age_param_2_sex_effects;
     vector<lower=0>[2] age_params[4];
 }
 
@@ -22,12 +27,18 @@ model{
     // Convenience vector
     vector[N_data] p;
     int index;
+    real end_prob;
+    real age_param_1;
+    real age_param_2;
     vector[2] mu;
     matrix[2, 2] cov;
 
     // Priors
     start_prob ~ beta(1.4, 1.4);
-    end_prob ~ beta(0.75, 0.75);
+    end_probs ~ beta(0.75, 0.75);
+    end_prob_sex_effects ~ normal(0, 0.75);
+    age_param_1_sex_effects ~ normal(0, 0.75);
+    age_param_2_sex_effects ~ normal(0, 0.75);
 
     // Joint prior
     mu[1] = 10;
@@ -41,7 +52,10 @@ model{
     // Likelihood
     for(i in 1:N_data) {
 	index = 2*(culture[part_id[i]] - 1) + condition[i];
-        p[i] = start_prob + (end_prob[index] - start_prob)*inv_logit(age_params[index, 2]*(age[part_id[i]] - age_params[index, 1]));
+	end_prob = inv_logit(logit(end_probs[index]) + end_prob_sex_effects[index]*sex[part_id[i]]);
+	age_param_1 = age_params[index, 1] + age_param_1_sex_effects[index]*sex[part_id[i]];
+	age_param_2 = age_params[index, 2] + age_param_2_sex_effects[index]*sex[part_id[i]];
+	p[i] = start_prob + (end_prob - start_prob)*inv_logit(age_param_2*(age[part_id[i]] - age_param_1));
     }
     shared ~ bernoulli(p);
 }
@@ -50,9 +64,15 @@ generated quantities {
     vector[N_data] log_lik;
     vector[N_data] p;
     int index;
+    real end_prob;
+    real age_param_1;
+    real age_param_2;
     for(i in 1:N_data) {
 	index = 2*(culture[part_id[i]] - 1) + condition[i];
-        p[i] = start_prob + (end_prob[index] - start_prob)*inv_logit(age_params[index, 2]*(age[part_id[i]] - age_params[index, 1]));
+	end_prob = inv_logit(logit(end_probs[index]) + end_prob_sex_effects[index]*sex[part_id[i]]);
+	age_param_1 = age_params[index, 1] + age_param_1_sex_effects[index]*sex[part_id[i]];
+	age_param_2 = age_params[index, 2] + age_param_2_sex_effects[index]*sex[part_id[i]];
+	p[i] = start_prob + (end_prob - start_prob)*inv_logit(age_param_2*(age[part_id[i]] - age_param_1));
         log_lik[i] = bernoulli_lpmf(shared[i] | p[i]);
     }
 }
